781e214305c9c4c30d2977726afd287b42a17c4a
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-08-29 14:54:45 +0300

    ops: change the directory for the root files

---
+++ src/root/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { App } from "../app";
+
+const rootElement = document.getElementById("root") as HTMLDivElement;
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
