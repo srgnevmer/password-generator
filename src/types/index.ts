746ff8c165833fd8c97b873d8af5a74ca46653cf
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-09-02 18:50:18 +0300

    refactor: add export for the Keys type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,11 @@ type Length = {
   length: number;
 };
 
-type Keys = "upperCase" | "lowerCase" | "numbers" | "brackets" | "symbols";
+export type Keys =
+  | "upperCase"
+  | "lowerCase"
+  | "numbers"
+  | "brackets"
+  | "symbols";
 
 export type Password = Length & Record<Keys, boolean>;
