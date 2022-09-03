fe6481bd21adef45aae6185cac42927ac381a4c0
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-09-03 19:31:36 +0300

    feat: change the value when need to modify the string

--- src/utils/modifyString.ts
+++ src/utils/modifyString.ts
@@ -1,4 +1,4 @@
 export const modifyString = (value: string): string => {
-  if (value.length <= 20) return value;
-  return value.slice(0, 20).concat("...");
+  if (value.length <= 15) return value;
+  return value.slice(0, 15).concat("...");
 };
