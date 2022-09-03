d9b5968aff0bc15355d9295da339a0e4b02773e5
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-09-03 19:39:57 +0300

    feat: add interface for PasswordConfig

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,8 @@ export type Keys =
   | "symbols";
 
 export type Password = Length & Record<Keys, boolean>;
+
+export interface PasswordConfig {
+  length: number;
+  characters: Record<Keys, boolean>;
+}
