538656259a7309554eb7170ed28becc5e0a6094b
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-11-27 22:17:01 +0200

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,70 @@
+export const MIN_WINDOW_WIDTH: number = 1024;
+
+export const DELAY: number = 400;
+
+export const REPOSITORY_LINK: string =
+  "https://github.com/srgnevmer/password-generator";
+
+export const NUMBERS: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export const SYMBOLS: string[] = ["!", "?", "@", "#", "$", "%", "&"];
+
+export const BRACKETS: string[] = ["(", ")", "[", "]", "{", "}"];
+
+export const UPPERCASE: string[] = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+export const LOWERCASE: string[] = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
