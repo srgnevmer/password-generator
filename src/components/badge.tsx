07073b8faf96ed25a8f7502c36383f511d8a41a6
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-11-27 21:54:18 +0200

    feat: add a badge

---
+++ src/components/badge.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+export const Badge: FC = () => {
+  return (
+    <div
+      className="flex justify-center items-center select-none
+      w-32 h-14 bg-blue-600 text-white font-semibold rounded-xl px-2"
+    >
+      VERY STRONG
+    </div>
+  );
+};
