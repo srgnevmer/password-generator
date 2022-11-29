295aaa5885ac75f6ee0c0e760126b2def3d4fcae
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-11-29 22:28:18 +0200

    feat: add password length

---
+++ src/ui/password-length.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { useAppSelector } from "../redux/typed-hooks";
+
+export const PasswordLength: FC = () => {
+  const passwordLength = useAppSelector<number>((state) => state.range.length);
+
+  return (
+    <p className="text-2xl text-white font-semibold mb-4">
+      Password length: {passwordLength}
+    </p>
+  );
+};
