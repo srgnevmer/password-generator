43e997b33d4ad889657a9c37c522545946cb7220
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-11-29 22:27:03 +0200

    feat: wrap the button in a memo & add prop func

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,21 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 
 type ButtonType = "primary" | "secondary";
 
 interface ButtonProps {
   text: string;
   type: ButtonType;
+  func?: () => void;
 }
 
-export const Button: FC<ButtonProps> = ({ text, type }) => {
+export const Button: FC<ButtonProps> = memo(({ text, type, func }) => {
   return (
     <button
+      onClick={func}
       className={`${type} text-white font-semibold rounded-xl shrink-0 bg-blue-600
     hover:bg-blue-700 active:bg-blue-600 transition duration-100`}
     >
       {text}
     </button>
   );
-};
+});
