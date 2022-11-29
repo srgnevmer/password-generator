99c5ef81c8a6a15d422f2a0c69f01a8b02794317
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-11-29 16:42:30 +0200

    feat: add a method to activate or deactivate the checkbox

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,12 +1,25 @@
 import { FC } from "react";
-import { SvgSelector } from "./index";
+import { CheckboxId } from "../types";
+import { useAppDispatch, useAppSelector } from "../redux/typed-hooks";
+import { activate, deactivate } from "../redux/slices/checkboxes-slice";
 
 interface CheckboxProps {
-  id: string;
+  id: CheckboxId;
   text: string;
 }
 
 export const Checkbox: FC<CheckboxProps> = ({ id, text }) => {
+  const dispatch = useAppDispatch();
+  const isActive = useAppSelector<boolean>((state) => state.checkboxes[id]);
+
+  const activateOrDeactivate = (): void => {
+    if (isActive) {
+      dispatch(deactivate(id));
+    } else {
+      dispatch(activate(id));
+    }
+  };
+
   return (
     <label
       htmlFor={id}
@@ -17,8 +30,10 @@ export const Checkbox: FC<CheckboxProps> = ({ id, text }) => {
         {text}
       </span>
       <input
-        type="checkbox"
         id={id}
+        type="checkbox"
+        checked={isActive}
+        onChange={activateOrDeactivate}
         className="w-10 h-10 border-2 rounded-xl bg-slate-800 cursor-pointer 
         focus:ring-offset-transparent focus:ring-transparent"
       />
