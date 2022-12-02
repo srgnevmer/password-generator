51b9b39e87a660a8fa5472d9a7a00fdaee4feeb1
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-02 14:37:28 +0200

    fix: when the length is min or max, the value does not change

--- src/redux/slices/range-slice.ts
+++ src/redux/slices/range-slice.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH } from "../../constants";
 
 export interface RangeState {
   length: number;
@@ -16,9 +17,11 @@ const rangeSlice = createSlice({
       state.length = action.payload;
     },
     lengthIncrement: (state: RangeState) => {
+      if (state.length === MAX_PASSWORD_LENGTH) return;
       state.length += 1;
     },
     lengthDecrement: (state: RangeState) => {
+      if (state.length === MIN_PASSWORD_LENGTH) return;
       state.length -= 1;
     },
   },
