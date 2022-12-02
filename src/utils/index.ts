fd92e0e38794d42c49f4d131e25f5c5f2ef66dd5
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-02 14:40:48 +0200

    feat: add export of the line trimming function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./get-width-window";
 export * from "./get-generated-password";
 export * from "./get-list-selected-characters";
 export * from "./get-random-number";
+export * from "./trim-string";
