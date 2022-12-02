e8321d5a86068c3a5f8bc1cd324dff4568036d9f
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-02 14:50:14 +0200

    feat: add password generation option

--- src/ui/password-section.tsx
+++ src/ui/password-section.tsx
@@ -1,20 +1,43 @@
-import { FC } from "react";
-import { SvgSelector, Badge } from "../components";
+import { FC, useState, useEffect, useMemo, useCallback } from "react";
+import { CheckboxesState } from "../types";
+import { MAX_LENGTH } from "../constants";
+import { IconButton, SvgSelector, Badge } from "../components";
+import { useAppSelector } from "../redux/typed-hooks";
+import { getGeneratedPassword, trimStr } from "../utils";
 
 export const PasswordSection: FC = () => {
+  const [generatePass, setGeneratePass] = useState<number>(0);
+  const [password, setPassword] = useState<string>("");
+  const selectedCheckboxes = useAppSelector<CheckboxesState>(
+    (state) => state.checkboxes
+  );
+  const passwordLength = useAppSelector<number>((state) => state.range.length);
+
+  useEffect(() => {
+    setPassword(getGeneratedPassword(selectedCheckboxes, passwordLength));
+  }, [generatePass, selectedCheckboxes, passwordLength]);
+
+  const refreshIcon = useMemo((): JSX.Element => {
+    return <SvgSelector id="refresh" />;
+  }, []);
+
+  const generatePassword = useCallback((): void => {
+    setGeneratePass((prevValue: number) => prevValue + 1);
+  }, []);
+
   return (
     <div
-      className="w-full max-w-4xl h-24 bg-slate-700 rounded-xl mb-6
-      flex items-center"
+      className="w-full max-w-4xl h-24 bg-slate-700 
+      rounded-xl mb-6 flex items-center"
     >
       <div className="w-full max-w-2xl h-full flex items-center px-8">
         <p className="text-2xl text-white font-semibold">
-          Tt1skQVrsvfRIOh6hlNRB2PdV
+          {trimStr(password, MAX_LENGTH)}
         </p>
       </div>
       <div className="flex justify-around items-center grow">
         <Badge />
-        <SvgSelector id="refresh" />
+        <IconButton icon={refreshIcon} func={generatePassword} />
       </div>
     </div>
   );
