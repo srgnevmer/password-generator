2bf48a50c18e77c835c0df1954f2ed5c1c247ffd
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-03 12:58:33 +0200

    feat: add a select-none class

--- src/ui/password-section.tsx
+++ src/ui/password-section.tsx
@@ -28,7 +28,7 @@ export const PasswordSection: FC = () => {
   return (
     <div
       className="w-full max-w-4xl h-24 bg-slate-700 
-      rounded-xl mb-6 flex items-center"
+      rounded-xl mb-6 flex items-center select-none"
     >
       <div className="w-full max-w-2xl h-full flex items-center px-8">
         <p className="text-2xl text-white font-semibold">
