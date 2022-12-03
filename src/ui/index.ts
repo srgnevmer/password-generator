65ca8248b75fd63e569936f114eb914f139d965f
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-03 12:38:52 +0200

    refactor: changed the sequence

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,6 +1,6 @@
 export * from "./layout";
 export * from "./header";
 export * from "./description";
+export * from "./password-length";
 export * from "./password-section";
 export * from "./settings-section";
-export * from "./password-length";
