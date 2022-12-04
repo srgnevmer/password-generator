dcb95edf3e761a7bf5783f6144024c79e30cf3f1
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-04 17:25:50 +0200

    feat: add alert and password reducers

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import alertSlice from "./slices/alert-slice";
 import rangeSlice from "./slices/range-slice";
+import passwordSlice from "./slices/password-slice";
 import checkboxesSlice from "./slices/checkboxes-slice";
 import idLastSelectedCheckboxSlice from "./slices/id-last-selected-checkbox-slice";
 
 export const store = configureStore({
   reducer: {
+    alert: alertSlice,
     range: rangeSlice,
+    password: passwordSlice,
     checkboxes: checkboxesSlice,
     idLastSelectedCheckbox: idLastSelectedCheckboxSlice,
   },
