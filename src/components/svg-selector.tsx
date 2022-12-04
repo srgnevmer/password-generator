f7d32185c991d91823d13cb13e01939d5bb52f5c
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-04 17:21:28 +0200

    feat: add a check icon

--- src/components/svg-selector.tsx
+++ src/components/svg-selector.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 
+type IdSvg = "refresh" | "check";
+
 interface SvgSelectorProps {
-  id: "refresh";
+  id: IdSvg;
 }
 
 export const SvgSelector: FC<SvgSelectorProps> = ({ id }) => {
@@ -21,6 +23,23 @@ export const SvgSelector: FC<SvgSelectorProps> = ({ id }) => {
             strokeLinecap="round"
             strokeLinejoin="round"
             d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      );
+    case "check":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="#fff"
+          className="w-8 h-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
       );
