f609c168e77a367637ffc8cfc311f351917795e3
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-04 17:42:48 +0200

    refactor: change the way the password is generated

--- src/ui/password-section.tsx
+++ src/ui/password-section.tsx
@@ -1,29 +1,36 @@
-import { FC, useState, useEffect, useMemo, useCallback } from "react";
+import { FC, useEffect, useMemo, useCallback } from "react";
 import { CheckboxesState } from "../types";
 import { MAX_LENGTH } from "../constants";
 import { IconButton, SvgSelector, Badge } from "../components";
-import { useAppSelector } from "../redux/typed-hooks";
+import { useAppDispatch, useAppSelector } from "../redux/typed-hooks";
+import { setPassword } from "../redux/slices/password-slice";
 import { getGeneratedPassword, trimStr } from "../utils";
 
 export const PasswordSection: FC = () => {
-  const [generatePass, setGeneratePass] = useState<number>(0);
-  const [password, setPassword] = useState<string>("");
+  const dispatch = useAppDispatch();
+  const password = useAppSelector<string>(
+    (state) => state.password.generatedPassword
+  );
   const selectedCheckboxes = useAppSelector<CheckboxesState>(
     (state) => state.checkboxes
   );
   const passwordLength = useAppSelector<number>((state) => state.range.length);
 
-  useEffect(() => {
-    setPassword(getGeneratedPassword(selectedCheckboxes, passwordLength));
-  }, [generatePass, selectedCheckboxes, passwordLength]);
-
   const refreshIcon = useMemo((): JSX.Element => {
     return <SvgSelector id="refresh" />;
   }, []);
 
-  const generatePassword = useCallback((): void => {
-    setGeneratePass((prevValue: number) => prevValue + 1);
-  }, []);
+  const getPassword = (): string => {
+    return getGeneratedPassword(selectedCheckboxes, passwordLength);
+  };
+
+  const newPasswordGenerated = useCallback((): void => {
+    dispatch(setPassword(getPassword()));
+  }, [selectedCheckboxes, passwordLength]);
+
+  useEffect(() => {
+    dispatch(setPassword(getPassword()));
+  }, [selectedCheckboxes, passwordLength]);
 
   return (
     <div
@@ -37,7 +44,7 @@ export const PasswordSection: FC = () => {
       </div>
       <div className="flex justify-around items-center grow">
         <Badge />
-        <IconButton icon={refreshIcon} func={generatePassword} />
+        <IconButton icon={refreshIcon} func={newPasswordGenerated} />
       </div>
     </div>
   );
